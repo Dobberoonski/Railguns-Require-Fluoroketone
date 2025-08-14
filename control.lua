6451fb9c19ac583c6eefa2cbcb89cce5b4500050
Dobberoonski <versericaw@gmail.com>
2025-08-14 10:26:38 +1000

    refactoring

--- control.lua
+++ control.lua
@@ -1,18 +1,10 @@
 --control.lua
-script.on_init(function()
+local function RailgunsRequireFluoroketone_Init()
+    --RailgunsRequireFluoroketone_Init : ->
     if not storage.fluoroAssemblers then
         storage.fluoroAssemblers = {}
     end
-end)
-
-script.on_configuration_changed(function()
-    if not storage.fluoroAssemblers then
-        storage.fluoroAssemblers = {}
-    end
-end)
---yea just fuck DRY i suppose lol
---i'm WET
---WRITE EVERYTHING TOGETHER
+end
 
 local function IHATEROTATIONSGODDAMN(direction)
     --IHATEROTATIONSGODDAMN : defines.direction -> x, y
@@ -20,13 +12,13 @@ local function IHATEROTATIONSGODDAMN(direction)
     elseif direction == defines.direction.east then return -1, 0
     elseif direction == defines.direction.south then return 0, -1
     elseif direction == defines.direction.west then return 1, 0
-    --railguns can rotate in 8 directions. Lucky me.
+    --[[railguns can rotate in 8 directions. Lucky me.
+    --I could just disable the 8 way rotation flag in the railguns prototype.
+    --I thought about that a lot.]]
     elseif direction == defines.direction.northeast or direction == defines.direction.northwest then return 0, 1
     elseif direction == defines.direction.southeast or direction == defines.direction.southwest then return 0, -1
     end
 end
---I could just disable the 8 way rotation flag in the railguns prototype.
---I thought about that a lot.
 
 local function IHATEDIRECTIONSTOOFUCK(direction)
     --IHATEDIRECTIONSTOOFUCK : defines.direction - > defines.direction
@@ -39,29 +31,30 @@ local function IHATEDIRECTIONSTOOFUCK(direction)
     end
 end
 
+script.on_init(RailgunsRequireFluoroketone_Init)
+script.on_configuration_changed(RailgunsRequireFluoroketone_Init)
+
 script.on_event(defines.events.on_script_trigger_effect, function(event)
     if event.effect_id ~= "railgun-turret" then return end
-    local key = script.register_on_object_destroyed(event.source_entity)
-    local offsetX, offsetY = IHATEROTATIONSGODDAMN(event.source_entity.direction)
+    local railgun = event.source_entity
+    local offsetX, offsetY = IHATEROTATIONSGODDAMN(railgun.direction)
     local ass3 = game.get_surface(event.surface_index).create_entity{
         name = "fluoro-assembling-machine-3",
         position = {
-            event.source_entity.position.x + offsetX,
-            event.source_entity.position.y + offsetY
+            railgun.position.x + offsetX,
+            railgun.position.y + offsetY
         },
-        direction = IHATEDIRECTIONSTOOFUCK(event.source_entity.direction),
-        force = event.source_entity.force
+        direction = IHATEDIRECTIONSTOOFUCK(railgun.direction),
+        force = railgun.force
     }
     ass3.destructible = false
     ass3.minable_flag = false
+    local key = script.register_on_object_destroyed(railgun)
     storage.fluoroAssemblers[key] = ass3
 end)
 
-script.on_event(defines.events.on_object_destroyed, function(event)
-    local key = event.registration_number
-    if not storage.fluoroAssemblers[key] then return end--required if Maraxsis 1.30.49 installed.
-    --see bug report: https://github.com/notnotmelon/maraxsis/issues/323
-    --tl;dr Maraxsis will call this function whenever **any** entity is created. Lmfao.
-    storage.fluoroAssemblers[key].destroy()
-    storage.fluoroAssemblers[key] = nil
-end)
+if script.active_mods["maraxsis"] then
+    require("runtime/compatability_on_object_destroyed")
+else
+    require("runtime/on_object_destroyed")
+end
